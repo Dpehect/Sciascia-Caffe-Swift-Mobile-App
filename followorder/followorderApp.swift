342dcd44b369914e219322897e5e51603d739c2a
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2026-06-29 13:34:59 +0300

    Initial Commit

---
+++ followorder/followorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  followorderApp.swift
+//  followorder
+//
+//  Created by Yunus Emre Gürlek on 29.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct followorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ followorderTests/followorderTests.swift
@@ -0,0 +1,18 @@
+//
+//  followorderTests.swift
+//  followorderTests
+//
+//  Created by Yunus Emre Gürlek on 29.06.2026.
+//
+
+import Testing
+
+struct followorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
